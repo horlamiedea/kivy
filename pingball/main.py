73a2d79bc754831efb3e0e94e241aee0d92e2027
horlamiedea <horlamiedea@gmail.com>
2020-07-13 16:43:14 +0100

    made the paddles work well

--- pingball/main.py
+++ pingball/main.py
@@ -5,6 +5,9 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 
+class PongPaddle(Widget):
+    pass
+
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
@@ -14,12 +17,19 @@ class PongBall(Widget):
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
  # moving the ball by calling the move function
+
 class PongGame(Widget):
+
     ball =ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
     def serve_ball(self):
         self.ball.velocity = Vector(4,0).rotate(randint(0, 360))
+
     def update(self, dt):
         self.ball.move()
+
         #  bouncing the ball top and bottom
         if (self.ball.y < 0) or (self.ball.y > self.height -50):
             self.ball.velocity_y *= -1
@@ -27,6 +37,14 @@ class PongGame(Widget):
 # left and right
         if (self.ball.x < 0) or (self.ball.x > self.width -50):
             self.ball.velocity_x *= -1
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 1/4:
+            self.player1.center_y = touch.y
+
+        if touch.x > self.width * 3/4:
+            self.player2.center_y = touch.y
+        return super().on_touch_move(touch)
 
 
 class PongApp(App):
