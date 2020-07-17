3b7b1c124336f8c401c966fcb888a06962f2d90f
horlamiedea <horlamiedea@gmail.com>
2020-07-17 21:54:17 +0100

    movement is made possible on paint

--- PaintApp/main.py
+++ PaintApp/main.py
@@ -1,11 +1,21 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.graphics import Color, Ellipse, Line
 # RGBA used to change color and the last one is for opacity of the colors
 Window.clearcolor = (0,1,1,1)
 
 class PaintWindow(Widget):
-    pass
+    def on_touch_down(self, touch):
+        self.canvas.add(Color(rgb = (1,0,0)))
+        d = 30
+        self.canvas.add(Ellipse(pos=(touch.x - d/2, touch.y - d/2),size=(d, d)))
+        touch.ud['line'] = Line(points=(touch.x, touch.y))
+        self.canvas.add(touch.ud['line'])
+        print(touch)
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 class PaintApp(App):
     def build(self):
